4d5f51be82159699b47f1943122d4a9b632d54cf
Brennan <61703271+BrennanCode1@users.noreply.github.com>
2020-09-27 13:52:22 -0700

    Update Testing.py
    Forgot while statement oops Testing stuff

---
+++ Testing.py
@@ -0,0 +1,29 @@
+import os
+import time
+
+
+callN=0 
+
+
+
+while callN < 101 :
+    print ("Starting check")
+    callN+=1
+    #intiializeing code to answer phone call
+    #intislizing code to check if phone call answered
+    #check the number is 1 on the first call
+    if callN == 1:
+        print("1")
+        #do the automated voice
+        #check if voice call automation is over
+        #hang up phone call
+    elif callN == 2:
+        print("1")
+        #do the automated voice
+        #check if voice call automaiton is over
+        #hang up phone call
+    elif callN==3:  
+        print ("3")
+    #adding one to the count to loop again for the next call
+    #check if phone is hung up
+    #add alert if code breaks
